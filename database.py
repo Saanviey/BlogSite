706f3615ae3c6ce79d7e73d81e8d84f3baef7394
Saanviey <saanvieyadav@gmail.com>
2026-05-02 18:27:27 +0530

    async

--- database.py
+++ database.py
@@ -1,21 +1,17 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker , DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import  DeclarativeBase
 
-db_url = "sqlite:///./blog.db"
+db_url = "sqlite+aiosqlite:///./blog.db"
 
-engine= create_engine(db_url , connect_args={"check_same_thread" : False})
+engine= create_async_engine(db_url )
 
-SessionLocal = sessionmaker( autoflush=False ,bind=engine , autocommit = False)
+SessionLocal = async_sessionmaker( engine , class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    db= SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()    
+async def get_db():
+   async with SessionLocal() as session:
+       yield session
